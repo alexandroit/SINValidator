23c0b8fbcb304d9c84ef5248b1193960e8b85ede
Alexandro Marques <aleinfoit@gmail.com>
2018-09-17 01:27:22 -0400

    Add Tests

---
+++ SINValidator.Tests/SINValidatorTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SINValidator.Tests
+{
+    public class SINValidatorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
