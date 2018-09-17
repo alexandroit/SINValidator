d95eab942b272d053087322d023aa953b3e6a109
Alexandro Marques <aleinfoit@gmail.com>
2018-09-17 15:30:51 -0400

    Tests created

--- tests/SINValidator.Tests/SINValidatorTests.cs
+++ tests/SINValidator.Tests/SINValidatorTests.cs
@@ -1,14 +1,33 @@
-using System;
 using Xunit;
 
 namespace SINValidator.Tests
 {
     public class SINValidatorTests
     {
-        [Fact]
-        public void Test1()
+        //Dois returner true
+        [Theory]
+        [InlineData(346232051)]
+        [InlineData(242793008)]
+        [InlineData(500003231)]
+        public void SinValid(int number)
         {
+            //Fake SIN, mais vrai matematique
+            var result = new SIN(number).Validate();
+            Assert.True(result);
+        }
 
+        //Dois returner false
+        [Theory]
+        [InlineData(143001859)]
+        [InlineData(111111111)]
+        [InlineData(999999999)]
+        [InlineData(1)]
+        [InlineData(123)]
+        public void SinInvalid(int number)
+        {
+            //Fake SIN, mais faux matematique
+            var result = new SIN(number).Validate();
+            Assert.False(result);
         }
     }
 }
